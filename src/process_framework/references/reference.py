543dba1e162c3d53999578ff06dec04a57d55730
Simon Wilson <simon.wilson@historicengland.org.uk>
2025-08-19 10:30:54 +0100

    tweaks to ColumnReference; changed order of constructor args,  renamed 'name' -> 'column', added get_value method

--- src/process_framework/references/reference.py
+++ src/process_framework/references/reference.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import TypeVar, Generic, Type, Callable, Any
+from typing import Type, Callable, Any
 
 @dataclass
 class Reference[T]:
--- src/process_framework/references/reference_dataframe_column.py
+++ src/process_framework/references/reference_dataframe_column.py
@@ -1,23 +1,31 @@
 from .reference import Reference
 from pandas import Series, DataFrame
-from typing import Callable, Any
+from typing import Any
 
 class ColumnReference(Reference[Series]):
     """ a reference to a column in the value of a Reference[DataFrame] """
-    def __init__(self, name:str, df:Reference[DataFrame]):
-        self.name = name
+    def __init__(self, df:Reference[DataFrame], column:str):
         self._type = Series
         self.df = df
+        self.column = column
 
 
     def set(self, value: Series | None):
         assert self.df.has_value(), "referenced dataframe has no value; we can't assign a value to it"
         df = self.df.value
-        df[self.name] = df.index.map(value)
+        assert isinstance(df, DataFrame)
+        df[self.column] = df.index.map(value)
         super().set(value)
 
 
+    def get_value(self, throw_on_none: bool = False) -> Series | None:
+        assert self.df.has_value(), "referenced dataframe has no value; we can't get a value from it"
+        df = self.df.value
+        assert isinstance(df, DataFrame)
+        return df[self.column]
+    
+
     def __repr__(self):
-        return f"ColumnReference[{'DataFrame' if self.df.has_value() else 'None'}.{self.name}]({self.value!r})"
+        return f"ColumnReference[{'DataFrame' if self.df.has_value() else 'None'}.{self.column}]({self.value!r})"
     
 
