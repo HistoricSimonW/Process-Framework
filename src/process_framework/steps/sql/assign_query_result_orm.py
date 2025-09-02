583bdc32b9b7741134f7820c2510e9b56f65b909
Simon Wilson <simon.wilson@historicengland.org.uk>
2025-09-02 19:30:57 +0100

    fixed a logic error in `populate_metadata` and `get_in_column`

--- src/process_framework/steps/sql/assign_query_result_orm.py
+++ src/process_framework/steps/sql/assign_query_result_orm.py
@@ -10,6 +10,7 @@ from sqlalchemy.schema import CreateTable, DropTable
 from itertools import batched
 from contextlib import AbstractContextManager
 from dataclasses import dataclass
+from process_framework.references.reference import _repr
 
 MAX_IN_VALUES_LEN = 10_000
 TEMP_TABLE_NAME = '#TEMP_IDS'
@@ -28,52 +29,54 @@ class GetOrmQueryResult[T:(DataFrame, Series)](GetSqlQueryResultBase[T], ABC):
 
         # metadata (do this here so we fail early, rather than during pipeline execution)
         self.metadata = self.get_metadata()
-        self.in_column = self.get_in_column()
-    
+
 
     def has_ids(self) -> bool:
-        _ids = self._ids
-        return (
-            isinstance(_ids, (list, ColumnReference)) or
-            (isinstance(_ids, Reference) and _ids.is_instance_of((list, Series)))
-        )
-    
+        """ return `True` if this has a reference to `_ids`; don't test if the `_ids` are valid or not;
+            we don't need to test the type here; that's been done by Pydantic """
+        return self._ids is not None
+
 
     def get_ids(self) -> list|None:
         """ handle _ids from list or Reference[list] or Reference[Series] or None to list or None """
         _ids = self._ids
         if _ids is None or isinstance(_ids, list):
             return _ids
-        
+
         if isinstance(_ids, Reference) and _ids.is_instance_of(list):
             value = _ids.get_value()
             assert isinstance(value, list)# or value is None
             return value
-        
+
         if (isinstance(_ids, Reference) and _ids.is_instance_of(Series)) or isinstance(_ids, ColumnReference):
             value = _ids.get_value()
             if isinstance(value, Series):
                 return value.to_list()
             return None
-            
+
         raise Exception()
-    
-    
+
+
     def get_metadata(self) -> MetaData:
-        """ initialize an instance of `MetaData`, pass it to `populate_metadata` """
+        """ initialize an instance of `MetaData`, pass it to `populate_metadata`; set the `in_column` from the metadata """
         metadata = MetaData()
-        
+
         # populate query-specific metadata
         self.populate_metadata(metadata)
+
+        # set `in_column` once `metadata` is populated
+        self.in_column = self.get_in_column()
 
         # if we have _ids, we need a 'temp table' definition in our metadata
         if self.has_ids():
             self._temp_table = self._get_temp_table_metadata(metadata)
-        
+            print(self._temp_table)
+
         return metadata
-    
+
 
     def _get_temp_table_metadata(self, metadata):
+        # because this is claled within `get_metadata`, `in_column` must be set; we rely on `in_column` to define the type of _id
         return Table(
             TEMP_TABLE_NAME, metadata,
             Column(TEMP_TABLE_ID, self.get_in_column().type)    # id values in the temp table should be of the same type as the 'in_column'
@@ -107,19 +110,19 @@ class GetOrmQueryResult[T:(DataFrame, Series)](GetSqlQueryResultBase[T], ABC):
             )
 
         return query
-    
+
 
     def get_query_result(self, query: Select, conn: Connection) -> DataFrame:
         # if we have _ids, execute the query in a temp table context managed by a context manager
         if (_ids := self.get_ids()):
-            
+
             with IdsTempTableContext(conn, self._temp_table, _ids):
                 return super().get_query_result(query, conn)
 
         # if we aren't handling _ids, we don't need any special logic
         return super().get_query_result(query, conn)
 
-        
+
 @dataclass
 class IdsTempTableContext(AbstractContextManager):
     """ context manager for temp-table using queries
@@ -133,6 +136,6 @@ class IdsTempTableContext(AbstractContextManager):
         self.conn.execute(CreateTable(self.table))
         for batch in batched(self._ids, 1000):
             self.conn.execute(insert(self.table).values([(t,) for t in batch]))
-    
+
     def __exit__(self, exc_type: type[BaseException] | None, exc_value: BaseException | None, traceback: TracebackType | None) -> None:
         self.conn.execute(DropTable(self.table))
