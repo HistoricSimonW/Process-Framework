87d48e95ddb51fc7881ac36a90285192e76bfaa3
Simon Wilson <simon.wilson@historicengland.org.uk>
2025-09-09 15:02:29 +0100

    updated `IndexDocuments` to accept optional `assign_result`

--- src/process_framework/steps/elasticsearch/index_documents.py
+++ src/process_framework/steps/elasticsearch/index_documents.py
@@ -5,12 +5,13 @@ from .document import Document
 from pandas import Series
 from elasticsearch.client import Elasticsearch
 from elasticsearch.helpers import bulk
-from typing import Any
+from typing import Any, Tuple
+from logging import info
 
 # https://elasticsearch-py.readthedocs.io/en/v8.2.2/helpers.html
 class IndexDocuments(Step):
     """ index a `Series` of `Documents` using elasticsearch.helpers.bulk """
-    def __init__(self, subject:Reference[Series], elasticsearch:Elasticsearch, index:str, pipeline:str|None=None, *, assert_index_exists:bool=True,
+    def __init__(self, subject:Reference[Series], elasticsearch:Elasticsearch, index:str, pipeline:str|None=None, *, assign_result:Reference[Tuple]|None=None, assert_index_exists:bool=True,
                  raise_on_error:bool=True, raise_on_exception:bool=True, max_retries:int=0, initial_backoff:int=2, 
                  chunk_size:int=500, max_chunk_bytes:int=104857600, bulk_kwargs:dict[str, Any]|None=None):
         super().__init__()
@@ -19,6 +20,7 @@ class IndexDocuments(Step):
         self.index = index
         self.pipeline= pipeline
         self.assert_exists=assert_index_exists
+        self.assign_result = assign_result
 
         # initialize bulk_kwargs with the provided dict, or an empty one
         self.bulk_kwargs = bulk_kwargs if isinstance(bulk_kwargs, dict) else dict()
@@ -57,4 +59,7 @@ class IndexDocuments(Step):
         )
         
         # I don't do anything with the result (yet)
-        print(result)
+        if self.assign_result is not None:
+            self.assign_result.set(result)
+            
+        info(f'{result}')
