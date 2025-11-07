0206650782031d89ca63a5bcf86150e6fc170a21
Simon Wilson <simon.wilson@historicengland.org.uk>
2025-11-07 21:45:19 +0000

    fixed implementation of SqlCredential to overwrite __client_from_kwargs__

--- src/process_framework/credentials/sql_credential.py
+++ src/process_framework/credentials/sql_credential.py
@@ -9,11 +9,12 @@ class SqlCredential(ConstructingCredentialModel[Engine]):
     QUERY_DRIVER:str|None=None
     USERNAME:str|None=None
     PASSWORD:str|None=None
+   
 
-    def get_client(self) -> Engine:
-        # construct a SqlAlchemy engine, applying some extra logic to handle the nested 'query.driver', which is required for SQL Server
-        args:dict[str, Any] = {k.lower():v for k, v in self.model_dump(exclude_none=True, exclude={'QUERY_DRIVER'})}
-        if self.QUERY_DRIVER:
-            args['query'] = dict(driver=self.QUERY_DRIVER)
+    @classmethod
+    def __client_from_kwargs__(cls, **kwargs) -> Engine:
+        args = {k:v for k, v in kwargs.items() if k not in {'query_driver'}}
+        if 'query_driver' in kwargs:
+            args['query'] = dict(driver=kwargs['query_driver'])
         url = URL.create(**args)
         return create_engine(url)
