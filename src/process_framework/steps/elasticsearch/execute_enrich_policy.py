ad0e1011fdd0ee44fe4ce898a9596a259479f02f
Simon Wilson <simon.wilson@historicengland.org.uk>
2025-11-13 15:19:37 +0000

    added some 'await task' logic to 'execute enrich policy'

--- src/process_framework/steps/elasticsearch/execute_enrich_policy.py
+++ src/process_framework/steps/elasticsearch/execute_enrich_policy.py
@@ -1,25 +1,46 @@
 from ..step import Step
 from elasticsearch.client import Elasticsearch
+from elasticsearch import NotFoundError
 from logging import info
+from itertools import count
+from time import sleep
 
 # https://elasticsearch-py.readthedocs.io/en/v8.2.2/api.html?highlight=execute#elasticsearch.client.EnrichClient.execute_policy:~:text=the%20enrich%20policy-,execute,_policy,-(*%2C%20name%3A%20str
 class ExecutePolicy(Step):
     """ execute the specified enrich policy """
-    def __init__(self, elasticsearch:Elasticsearch, policy:str, master_timeout:str='15s') -> None:
+    def __init__(self, elasticsearch:Elasticsearch, policy:str, await_task:bool=True, await_task_interval:float=1, await_task_timeout:int=120) -> None:
         self.elasticsearch = elasticsearch
         self.policy = policy
-        self.master_timeout = master_timeout
+        self.await_task = await_task
+        self.await_task_interval = await_task_interval
+        self.await_task_timeout = await_task_timeout
 
 
     def do(self):
         enrich = self.elasticsearch.enrich
         assert enrich.get_policy(name=self.policy)
-        enrich.execute_policy(
+        response = enrich.execute_policy(
             name=self.policy,
-            master_timeout=self.master_timeout
+            wait_for_completion=False
         )
 
-        enrich.execute_policy(name=self.policy)
+        info(f'executed policy {self.policy}:({response})')
+
+        if not self.await_task:
+            return None
+        
+        task = response.body['task']
+
+        # for 0 .. await_task_timeout, try to get the task
+        #   if the task exists, the task is running
+        #   if the task is not found, it's finished
+        for _ in range(self.await_task_timeout):
+            try:
+                self.elasticsearch.tasks.get(task_id=task)
+            except NotFoundError:
+                break
+            sleep(self.await_task_interval)
+                
         info(f'executed enrich policy {self.policy}')
 
     
