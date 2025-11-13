d1f096caf3ce353d508f40efb76a2b7b95090980
Simon Wilson <simon.wilson@historicengland.org.uk>
2025-11-13 12:33:35 +0000

    added master_timeout to ExecutePolicy __init__ Updated steps.elasticsearch init

--- src/process_framework/steps/elasticsearch/__init__.py
+++ src/process_framework/steps/elasticsearch/__init__.py
@@ -3,3 +3,5 @@ from .documents_from_dataframe import DataFrameToDocuments
 from .index_documents import IndexDocuments
 from .assign_scan_result import ScanToDataFrame
 from .delete_by_id import DeleteById
+from .execute_enrich_policy import ExecutePolicy
+from .update_by_query import UpdateByQuery
--- src/process_framework/steps/elasticsearch/execute_enrich_policy.py
+++ src/process_framework/steps/elasticsearch/execute_enrich_policy.py
@@ -5,16 +5,18 @@ from logging import info
 # https://elasticsearch-py.readthedocs.io/en/v8.2.2/api.html?highlight=execute#elasticsearch.client.EnrichClient.execute_policy:~:text=the%20enrich%20policy-,execute,_policy,-(*%2C%20name%3A%20str
 class ExecutePolicy(Step):
     """ execute the specified enrich policy """
-    def __init__(self, elasticsearch:Elasticsearch, policy:str) -> None:
+    def __init__(self, elasticsearch:Elasticsearch, policy:str, master_timeout:str='15s') -> None:
         self.elasticsearch = elasticsearch
         self.policy = policy
+        self.master_timeout = master_timeout
 
 
     def do(self):
         enrich = self.elasticsearch.enrich
         assert enrich.get_policy(name=self.policy)
         enrich.execute_policy(
-            name=self.policy
+            name=self.policy,
+            master_timeout=self.master_timeout
         )
 
         enrich.execute_policy(name=self.policy)
