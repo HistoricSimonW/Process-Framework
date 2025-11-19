e8c730ba9c25a186cb84288c192d89c0c558b8e8
Simon Wilson <simon.wilson@historicengland.org.uk>
2025-11-19 14:48:57 +0000

    added TrustServerCertificate to SqlCredential

--- src/process_framework/credentials/sql_credential.py
+++ src/process_framework/credentials/sql_credential.py
@@ -7,6 +7,7 @@ class SqlCredential(ConstructingCredentialModel[Engine]):
     HOST:str
     DATABASE:str
     QUERY_DRIVER:str|None=None
+    QUERY_TRUST_SERVER_CERTIFICATE:str|None=None
     USERNAME:str|None=None
     PASSWORD:str|None=None
    
@@ -16,5 +17,7 @@ class SqlCredential(ConstructingCredentialModel[Engine]):
         args = {k:v for k, v in kwargs.items() if k not in {'query_driver'}}
         if 'query_driver' in kwargs:
             args['query'] = dict(driver=kwargs['query_driver'])
+            if kwargs.get('query_trust_server_certificate'):
+                args['query']['TrustServerCertificate'] = kwargs['query_trust_server_certificate']
         url = URL.create(**args)
         return create_engine(url)
