a4bd3e37ab8d6e2a8a0bd24aa8b8c46e699734b4
Simon Wilson <simon.wilson@historicengland.org.uk>
2026-07-02 17:22:19 +0100

    fixed ListAccumulator

--- src/process_framework/references/collections.py
+++ src/process_framework/references/collections.py
@@ -4,13 +4,19 @@ from ..composition.core.logging import HasLogger
 from typing import Iterable
 
 @dataclass(slots=True)
-class ListAccumulator[T](IGettable[Iterable[T]], ISettable[T], HasLogger):
+class ListAccumulator[T](IGettable[Iterable[T]], ISettable[Iterable], HasLogger):
     item_type:type[T]
     values:list = field(default_factory=list)
 
-    def _on_set(self, value: T | None) -> None:
-        self.values.append(value)
-        self._info(f'+{value} -> ({len(self.values)})')
+    def get_type(self) -> type:
+        return type(self.values)
+
+    def _on_set(self, value: Iterable | None) -> None:
+        if value is not None:
+            pre = len(self.values)
+            self.values += [t for t in value if isinstance(t, self.item_type)]
+            post = len(self.values)
+            self._info(f'{pre} -> {post}')
 
     def get_value(self) -> list[T]:
         return self.values
