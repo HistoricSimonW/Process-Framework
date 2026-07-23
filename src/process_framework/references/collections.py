f5b7427d79ad2834c3367201f02d780e7ed67e8b
Simon Wilson <simon.wilson@historicengland.org.uk>
2026-07-23 14:41:39 +0100

    Removed duplicate ListExtender definition

--- src/process_framework/references/collections.py
+++ src/process_framework/references/collections.py
@@ -31,26 +31,6 @@ class ListExtender[T](IGettable[list[T]], ISettable[Iterable[T]], HasLogger):
     
     def get_value(self) -> list[T]:
         return self.values
-
-
-@dataclass(slots=True)
-class ListExtender[T](IGettable[list[T]], ISettable[Iterable[T]], HasLogger):
-    """ extend a list of Ts with an Iterable of Ts """
-    item_type:type[T]
-    values:list[T] = field(default_factory=list)
-    
-    def _on_set(self, value: Iterable[T] | None) -> None:
-        pre = len(self.values)
-        if value is not None:
-            self.values.extend((t for t in value if isinstance(t, self.item_type)))
-        post = len(self.values)
-        self._info(f'{pre} -> {post}')
-    
-    def get_type(self) -> type:
-        return type(self.values)
-    
-    def has_value(self) -> bool:
-        return self.values is not None
     
     
 @dataclass(slots=True)
